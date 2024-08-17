2b2cdefae6c57ee4994e0ee70a111e5264f617e9
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2024-08-17 13:40:37 -0400

    adding pin icons to map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './map.scss'
 import "leaflet/dist/leaflet.css";
+import Pin from '../pin/Pin';
 
 function Map({items}){
   return (
@@ -9,6 +10,9 @@ function Map({items}){
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
+    {items.map(item=>(
+      <Pin item={item} key={item.id}/>
+    ))}
   </MapContainer>
   )
 }
